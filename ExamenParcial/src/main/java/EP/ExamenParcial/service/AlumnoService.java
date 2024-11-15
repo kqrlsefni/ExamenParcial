8512aee3be1aa08760654c53dbee75d3bfd8b885
kqrlsefni <daremonemo@gmail.com>
2024-11-15 13:46:18 -0500

    AlumnoService implementation

---
+++ ExamenParcial/src/main/java/EP/ExamenParcial/service/AlumnoService.java
@@ -0,0 +1,42 @@
+package EP.ExamenParcial.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import EP.ExamenParcial.model.AlumnoModel;
+import EP.ExamenParcial.repository.IAlumnoRepository;
+
+public class AlumnoService implements IAlumnoService{
+
+    @Autowired
+    IAlumnoRepository alumnoRepository;
+
+    @Override
+    public List<AlumnoModel> findAll() {
+       return (List<AlumnoModel>) alumnoRepository.findAll();
+    }
+
+    @Override
+    public Optional<AlumnoModel> findById(Integer id) {
+        return alumnoRepository.findById(id);
+    }
+
+    @Override
+    public AlumnoModel add(AlumnoModel model) {
+        return alumnoRepository.save(model);
+    }
+
+    @Override
+    public AlumnoModel update(AlumnoModel model) {
+        return alumnoRepository.save(model);
+    }
+
+    @Override
+    public Boolean delete(Integer id) {
+        alumnoRepository.deleteById(id);
+        return true;
+    }
+    
+}
--- ExamenParcial/src/main/java/EP/ExamenParcial/service/IAlumnoService.java
+++ ExamenParcial/src/main/java/EP/ExamenParcial/service/IAlumnoService.java
@@ -1,13 +1,14 @@
 package EP.ExamenParcial.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import EP.ExamenParcial.model.AlumnoModel;
 
 public interface IAlumnoService {
     
     public List<AlumnoModel> findAll();
-    public AlumnoModel findById();
+    public Optional<AlumnoModel> findById(Integer id);
     public AlumnoModel add(AlumnoModel model);
     public AlumnoModel update(AlumnoModel model);
     public Boolean delete(Integer id);
